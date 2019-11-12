87c0c57e1a56780e139dd319c2d3c12b575d3b64
Sergei Mikhailovskii <mikhailovskii.s@tut.by>
2019-11-12 21:11:52 +0300

    Added database

--- src/main/java/contoller/LoginController.java
+++ src/main/java/contoller/LoginController.java
@@ -6,11 +6,10 @@ public class LoginController {
     private LoginWindow window;
 
     public void logInUser(String login, String password) {
-        System.out.println(login+" "+password);
-        window.showDialog("Logged in");
+        System.out.println(login + " " + password);
     }
 
-    public void attachView(LoginWindow window){
+    public void attachView(LoginWindow window) {
         this.window = window;
     }
 }
