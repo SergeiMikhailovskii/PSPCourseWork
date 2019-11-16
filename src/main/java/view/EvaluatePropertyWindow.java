d4d3a78c4a19532ea07af90a47123729fc821134
Sergei Mikhailovskii <mikhailovskii.s@tut.by>
2019-11-16 21:11:42 +0300

    Refactored EvaluatePropertyWindow design

--- src/main/java/view/EvaluatePropertyWindow.java
+++ src/main/java/view/EvaluatePropertyWindow.java
@@ -4,12 +4,19 @@ import javax.swing.*;
 import java.awt.*;
 
 public class EvaluatePropertyWindow extends JFrame {
-    private String[] buildingTypes = {"Stock", "Flat"};
-    private JComboBox<String> buildingTypeJCB = new JComboBox<>(buildingTypes);
-    private JTextField yearProfitTF = new JTextField();
-    private JTextField buildCostsTF = new JTextField();
-    private JTextField operatingCostsTF = new JTextField();
-    private JTextField capitalizationPercentTF = new JTextField();
+
+    private JLabel addressLB = new JLabel("Address: ");
+    private JTextField addressTF = new JTextField();
+    private JLabel squareLB = new JLabel("Square: ");
+    private JTextField squareTF = new JTextField();
+    private JLabel totalLB = new JLabel("Total: ");
+    private JLabel distanceFromCenterLB = new JLabel("Distance from center: ");
+    private JTextField distanceFromCenterTF = new JTextField();
+    private JLabel buildYearLB = new JLabel("Year of build: ");
+    private JTextField buildYearTF = new JTextField();
+    private JLabel repairDegreeLB = new JLabel("Repair degree:");
+    private JTextField repairDegreeTF = new JTextField();
+    private JButton calculateBtn = new JButton("Calculate");
 
     EvaluatePropertyWindow() {
         super("Evaluate Property");
@@ -21,31 +28,60 @@ public class EvaluatePropertyWindow extends JFrame {
         SpringLayout springLayout = new SpringLayout();
         getContentPane().setLayout(springLayout);
 
-        getContentPane().add(buildingTypeJCB);
-        springLayout.putConstraint(SpringLayout.HORIZONTAL_CENTER, buildingTypeJCB, 0, SpringLayout.HORIZONTAL_CENTER, getContentPane());
-        springLayout.putConstraint(SpringLayout.NORTH, buildingTypeJCB, 20, SpringLayout.NORTH, getContentPane());
+        getContentPane().add(addressLB);
+        springLayout.putConstraint(SpringLayout.WEST, addressLB, 20, SpringLayout.WEST, getContentPane());
+        springLayout.putConstraint(SpringLayout.NORTH, addressLB, 20, SpringLayout.NORTH, getContentPane());
 
-        getContentPane().add(yearProfitTF);
-        yearProfitTF.setPreferredSize(new Dimension(200,20));
-        springLayout.putConstraint(SpringLayout.HORIZONTAL_CENTER, yearProfitTF, 0, SpringLayout.HORIZONTAL_CENTER, getContentPane());
-        springLayout.putConstraint(SpringLayout.NORTH, yearProfitTF, 20, SpringLayout.SOUTH, buildingTypeJCB);
+        getContentPane().add(addressTF);
+        addressTF.setPreferredSize(new Dimension(200,20));
+        springLayout.putConstraint(SpringLayout.WEST, addressTF, 20, SpringLayout.EAST, addressLB);
+        springLayout.putConstraint(SpringLayout.NORTH, addressTF, 20, SpringLayout.NORTH, getContentPane());
 
-        getContentPane().add(buildCostsTF);
-        buildCostsTF.setPreferredSize(new Dimension(200,20));
-        springLayout.putConstraint(SpringLayout.HORIZONTAL_CENTER, buildCostsTF, 0, SpringLayout.HORIZONTAL_CENTER, getContentPane());
-        springLayout.putConstraint(SpringLayout.NORTH, buildCostsTF, 20, SpringLayout.SOUTH, yearProfitTF);
+        getContentPane().add(squareLB);
+        springLayout.putConstraint(SpringLayout.WEST, squareLB, 20, SpringLayout.WEST, getContentPane());
+        springLayout.putConstraint(SpringLayout.NORTH, squareLB, 20, SpringLayout.SOUTH, addressTF);
 
-        getContentPane().add(operatingCostsTF);
-        operatingCostsTF.setPreferredSize(new Dimension(200,20));
-        springLayout.putConstraint(SpringLayout.HORIZONTAL_CENTER, operatingCostsTF, 0, SpringLayout.HORIZONTAL_CENTER, getContentPane());
-        springLayout.putConstraint(SpringLayout.NORTH, operatingCostsTF, 20, SpringLayout.SOUTH, buildCostsTF);
+        getContentPane().add(squareTF);
+        squareTF.setPreferredSize(new Dimension(200,20));
+        springLayout.putConstraint(SpringLayout.WEST, squareTF, 20, SpringLayout.EAST, squareLB);
+        springLayout.putConstraint(SpringLayout.NORTH, squareTF, 20, SpringLayout.SOUTH, addressTF);
 
-        getContentPane().add(capitalizationPercentTF);
-        capitalizationPercentTF.setPreferredSize(new Dimension(200,20));
-        springLayout.putConstraint(SpringLayout.HORIZONTAL_CENTER, capitalizationPercentTF, 0, SpringLayout.HORIZONTAL_CENTER, getContentPane());
-        springLayout.putConstraint(SpringLayout.NORTH, capitalizationPercentTF, 20, SpringLayout.SOUTH, operatingCostsTF);
+        getContentPane().add(distanceFromCenterLB);
+        springLayout.putConstraint(SpringLayout.WEST, distanceFromCenterLB, 20, SpringLayout.WEST, getContentPane());
+        springLayout.putConstraint(SpringLayout.NORTH, distanceFromCenterLB, 20, SpringLayout.SOUTH, squareTF);
 
-        setSize(300, 300);
+        getContentPane().add(distanceFromCenterTF);
+        distanceFromCenterTF.setPreferredSize(new Dimension(200,20));
+        springLayout.putConstraint(SpringLayout.WEST, distanceFromCenterTF, 20, SpringLayout.EAST, distanceFromCenterLB);
+        springLayout.putConstraint(SpringLayout.NORTH, distanceFromCenterTF, 20, SpringLayout.SOUTH, squareTF);
+
+        getContentPane().add(buildYearLB);
+        springLayout.putConstraint(SpringLayout.WEST, buildYearLB, 20, SpringLayout.WEST, getContentPane());
+        springLayout.putConstraint(SpringLayout.NORTH, buildYearLB, 20, SpringLayout.SOUTH, distanceFromCenterTF);
+
+        getContentPane().add(buildYearTF);
+        buildYearTF.setPreferredSize(new Dimension(200,20));
+        springLayout.putConstraint(SpringLayout.WEST, buildYearTF, 20, SpringLayout.EAST, buildYearLB);
+        springLayout.putConstraint(SpringLayout.NORTH, buildYearTF, 20, SpringLayout.SOUTH, distanceFromCenterTF);
+
+        getContentPane().add(repairDegreeLB);
+        springLayout.putConstraint(SpringLayout.WEST, repairDegreeLB, 20, SpringLayout.WEST, getContentPane());
+        springLayout.putConstraint(SpringLayout.NORTH, repairDegreeLB, 20, SpringLayout.SOUTH, buildYearTF);
+
+        getContentPane().add(repairDegreeTF);
+        repairDegreeTF.setPreferredSize(new Dimension(200,20));
+        springLayout.putConstraint(SpringLayout.WEST, repairDegreeTF, 20, SpringLayout.EAST, repairDegreeLB);
+        springLayout.putConstraint(SpringLayout.NORTH, repairDegreeTF, 20, SpringLayout.SOUTH, buildYearTF);
+
+        getContentPane().add(calculateBtn);
+        springLayout.putConstraint(SpringLayout.HORIZONTAL_CENTER, calculateBtn, 0, SpringLayout.HORIZONTAL_CENTER, repairDegreeTF);
+        springLayout.putConstraint(SpringLayout.NORTH, calculateBtn, 20, SpringLayout.SOUTH, repairDegreeTF);
+
+        getContentPane().add(totalLB);
+        springLayout.putConstraint(SpringLayout.HORIZONTAL_CENTER, totalLB, 0, SpringLayout.HORIZONTAL_CENTER, repairDegreeTF);
+        springLayout.putConstraint(SpringLayout.NORTH, totalLB, 20, SpringLayout.SOUTH, calculateBtn);
+
+        setSize(450, 350);
     }
 
 }
