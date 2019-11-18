89ddcd5935969b923a96dbe001cad9a1cbd963c3
Sergei Mikhailovskii <mikhailovskii.s@tut.by>
2019-11-18 23:10:04 +0300

    Added chart screen

---
+++ src/main/java/view/ChartScreen.java
@@ -0,0 +1,39 @@
+package view;
+
+import org.jfree.chart.ChartFactory;
+import org.jfree.chart.ChartPanel;
+import org.jfree.chart.JFreeChart;
+import org.jfree.chart.ui.ApplicationFrame;
+import org.jfree.data.general.DefaultPieDataset;
+import org.jfree.data.general.PieDataset;
+
+import javax.swing.*;
+
+public class ChartScreen extends ApplicationFrame {
+
+    public ChartScreen(String title) {
+        super(title);
+        this.setSize(500, 500);
+        setContentPane(createDemoPanel());
+    }
+
+    private JPanel createDemoPanel() {
+        JFreeChart chart = createChart(createDataset());
+        return new ChartPanel(chart);
+    }
+
+    private JFreeChart createChart(PieDataset dataset) {
+        return ChartFactory.createPieChart("Flats", dataset, true, true, false);
+    }
+
+    private PieDataset createDataset() {
+        DefaultPieDataset dataset = new DefaultPieDataset();
+        dataset.setValue("1960-1970", 20);
+        dataset.setValue("1970-1980", 20);
+        dataset.setValue("1980-1990", 20);
+        dataset.setValue("1990-2000", 20);
+        dataset.setValue("2000-2010", 10);
+        dataset.setValue("2010-2020", 10);
+        return dataset;
+    }
+}
