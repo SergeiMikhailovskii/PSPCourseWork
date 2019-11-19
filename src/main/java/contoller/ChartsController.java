5c9616e2b26159dc9eda7e6afebb3d2129155405
Sergei Mikhailovskii <mikhailovskii.s@tut.by>
2019-11-19 23:39:24 +0300

    Moved DistanceFromCenterBarChart logic to controller

--- src/main/java/contoller/ChartsController.java
+++ src/main/java/contoller/ChartsController.java
@@ -1,6 +1,8 @@
 package contoller;
 
 import client.ClientSocket;
+import org.jfree.data.category.CategoryDataset;
+import org.jfree.data.category.DefaultCategoryDataset;
 import org.jfree.data.general.DefaultPieDataset;
 import view.DistanceFromCenterBarChartWindow;
 import view.YearPieChartWindow;
@@ -26,11 +28,32 @@ public class ChartsController {
     }
 
     private void showDistanceFromCenterBarChartWindow() {
-        new DistanceFromCenterBarChartWindow("Distance from center").setVisible(true);
+        new DistanceFromCenterBarChartWindow("Distance from center", getDistanceFromCenterBarChartData()).setVisible(true);
     }
 
     private void showYearPieChartWindow() {
         new YearPieChartWindow("Year chart", getBuildYearData()).setVisible(true);
+    }
+
+    private CategoryDataset getDistanceFromCenterBarChartData() {
+        final String distance1 = "0-2";
+        final String distance2 = "2-4";
+        final String distance3 = "4-6";
+        final String distance4 = "6-8";
+        final String distance5 = "8-10";
+        final String distance6 = "10-12";
+        final String distanceFromCenter = "";
+
+        final DefaultCategoryDataset dataset = new DefaultCategoryDataset();
+
+        dataset.addValue(10, distance1, distanceFromCenter);
+        dataset.addValue(20, distance2, distanceFromCenter);
+        dataset.addValue(30, distance3, distanceFromCenter);
+        dataset.addValue(30, distance4, distanceFromCenter);
+        dataset.addValue(5, distance5, distanceFromCenter);
+        dataset.addValue(5, distance6, distanceFromCenter);
+
+        return dataset;
     }
 
     private DefaultPieDataset getBuildYearData() {
--- src/main/java/view/DistanceFromCenterBarChartWindow.java
+++ src/main/java/view/DistanceFromCenterBarChartWindow.java
@@ -6,17 +6,18 @@ import org.jfree.chart.JFreeChart;
 import org.jfree.chart.plot.PlotOrientation;
 import org.jfree.chart.ui.ApplicationFrame;
 import org.jfree.data.category.CategoryDataset;
-import org.jfree.data.category.DefaultCategoryDataset;
 
 public class DistanceFromCenterBarChartWindow extends ApplicationFrame {
+    private CategoryDataset dataset;
 
-    public DistanceFromCenterBarChartWindow(String title) {
+    public DistanceFromCenterBarChartWindow(String title, CategoryDataset dataset) {
         super(title);
+        this.dataset = dataset;
         JFreeChart barChart = ChartFactory.createBarChart(
                 title,
                 "Distance in km",
                 "%",
-                createDataset(),
+                dataset,
                 PlotOrientation.VERTICAL,
                 true,
                 true,
@@ -28,24 +29,4 @@ public class DistanceFromCenterBarChartWindow extends ApplicationFrame {
         this.pack();
     }
 
-    private CategoryDataset createDataset() {
-        final String distance1 = "0-2";
-        final String distance2 = "2-4";
-        final String distance3 = "4-6";
-        final String distance4 = "6-8";
-        final String distance5 = "8-10";
-        final String distance6 = "10-12";
-        final String distanceFromCenter = "";
-
-        final DefaultCategoryDataset dataset = new DefaultCategoryDataset();
-
-        dataset.addValue(10, distance1, distanceFromCenter);
-        dataset.addValue(20, distance2, distanceFromCenter);
-        dataset.addValue(30, distance3, distanceFromCenter);
-        dataset.addValue(30, distance4, distanceFromCenter);
-        dataset.addValue(5, distance5, distanceFromCenter);
-        dataset.addValue(5, distance6, distanceFromCenter);
-
-        return dataset;
-    }
 }
