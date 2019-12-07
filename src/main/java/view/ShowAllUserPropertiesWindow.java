7f599fea64621e73fc0672e05b7755b8589571db
Sergei Mikhailovskii <mikhailovskii.s@tut.by>
2019-12-08 00:01:57 +0300

    Added search property

--- src/main/java/view/ShowAllUserPropertiesWindow.java
+++ src/main/java/view/ShowAllUserPropertiesWindow.java
@@ -4,12 +4,20 @@ import base.BaseView;
 import contoller.ShowAllUserPropertiesController;
 
 import javax.swing.*;
+import javax.swing.table.DefaultTableModel;
+import java.awt.*;
 import java.util.List;
 import java.util.Vector;
 
 public class ShowAllUserPropertiesWindow extends JFrame implements BaseView {
     private JTable table = new JTable();
+    private JButton findPropertyBtn = new JButton("Find property");
+    private JButton searchDlgBtn = new JButton("Search");
+    private JDialog dialog = new JDialog();
+    private JTextField dialogTF = new JTextField();
+
     private Vector<String> columnNames = new Vector<>();
+    private Vector<Vector> data;
 
     private ShowAllUserPropertiesController controller = new ShowAllUserPropertiesController();
 
@@ -26,6 +34,7 @@ public class ShowAllUserPropertiesWindow extends JFrame implements BaseView {
     }
 
     public void setTable(Vector<Vector> data) {
+        this.data = data;
         table = new JTable(data, columnNames);
     }
 
@@ -34,9 +43,67 @@ public class ShowAllUserPropertiesWindow extends JFrame implements BaseView {
         SpringLayout springLayout = new SpringLayout();
         getContentPane().setLayout(springLayout);
 
-        getContentPane().add(new JScrollPane(table));
+        JScrollPane scrollPane = new JScrollPane(table);
+        getContentPane().add(scrollPane);
         springLayout.putConstraint(SpringLayout.HORIZONTAL_CENTER, table, 0, SpringLayout.WEST, getContentPane());
         springLayout.putConstraint(SpringLayout.NORTH, table, 0, SpringLayout.NORTH, getContentPane());
+
+        getContentPane().add(findPropertyBtn);
+        springLayout.putConstraint(SpringLayout.HORIZONTAL_CENTER, findPropertyBtn, 0, SpringLayout.HORIZONTAL_CENTER, scrollPane);
+        springLayout.putConstraint(SpringLayout.NORTH, findPropertyBtn, 20, SpringLayout.SOUTH, scrollPane);
+        findPropertyBtn.addActionListener(e -> dialog.setVisible(true));
+
+        JPanel pane = new JPanel();
+        pane.setLayout(new FlowLayout());
+        pane.add(new JLabel("Enter the property address"));
+        dialogTF.setPreferredSize(new Dimension(100, 20));
+        pane.add(dialogTF);
+        pane.add(searchDlgBtn);
+        dialog.add(pane);
+        dialog.setPreferredSize(new Dimension(300, 300));
+
+        searchDlgBtn.addActionListener(e -> {
+            String address = dialogTF.getText();
+            if (address.equalsIgnoreCase("")) {
+                DefaultTableModel model = (DefaultTableModel) table.getModel();
+                int size = model.getRowCount();
+                for (int i = 0; i < size; i++) {
+                    model.removeRow(0);
+                }
+
+                for (Vector newDatum : this.data) {
+                    model.addRow(newDatum);
+                }
+                table.setModel(model);
+            } else {
+                Vector<Vector> newData = new Vector<>();
+                for (Vector vector : data) {
+                    if (vector.get(0).equals(address)) {
+                        newData.add(vector);
+                    }
+                }
+                DefaultTableModel model = (DefaultTableModel) table.getModel();
+                int size = model.getRowCount();
+                for (int i = 0; i < size; i++) {
+                    model.removeRow(0);
+                }
+
+                for (Vector newDatum : newData) {
+                    model.addRow(newDatum);
+                }
+                table.setModel(model);
+/*
+                table = new JTable(newData, columnNames);
+                for (Vector data : newData) {
+                    System.out.println(data.get(0));
+                }*/
+            }
+            dialog.setVisible(false);
+/*
+            getContentPane().validate();
+            getContentPane().repaint();
+*/
+        });
 
         setSize(500, 500);
     }
